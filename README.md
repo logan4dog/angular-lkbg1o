9dbdb4be0613da532761297983169c75561887de
logan4dog <logan4dog@gmail.com>
2020-10-01 18:20:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-lkbg1o-n2ysug
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-lkbg1o-n2ysug)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'It is a phone.'
+  },
+  {
+    name:'LG Phoenix 4',
+    price:58,
+    description:'The LG® Phoenix 4 (AT&T Prepaid) is smart, sleek and beautiful smartphone with 1.4 GHz Qualcomm processor powered by Android 7.0 Nougat OS. The smartphone has large and vivid 5" touchscreen HD display for remarkable detail and clarity. The smartphone allows crisp and immersive multimedia experience while viewing movies, videos or gaming. The smartphone captures photos with 8.0-megapixel rear camera and on a 5 MP front-facing camera. The smartphone offers 16 GB internal storage, which is expandable up to 32 GB (sold separately).'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
